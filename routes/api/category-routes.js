efb4c18472d3ca04db6ace27cdf974646aff6875
linbets <lindseybetsinger@gmail.com>
2021-11-17 19:56:54 -0600

    final

---
+++ routes/api/category-routes.js
@@ -0,0 +1,53 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  Category.findAll({
+    include: [Product]
+  }).then((all) => {
+    res.json(all)
+  })
+});
+
+router.get('/:id', (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  Category.findOne({
+    where: {id: req.params.id},
+    include: [Product]
+  }).then((all) => {
+    res.json(all)
+  })
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+  Category.create(req.body).then((all) => {
+    res.json(all)
+  })
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+  Category.update(
+    req.body,
+    {where: {id: req.params.id}},
+  ).then((all) => {
+    res.json(all)
+  })
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+  Category.destroy({
+    where: {id: req.params.id}
+  }).then((all) => {
+    res.json(all)
+  })
+});
+
+module.exports = router;
